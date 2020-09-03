58e4a3108a48f6b3f74092371c2e610c0f49c295
Parkduksung <duksung1234@naver.com>
2020-09-03 15:42:58 +0900

    android_developers_documents_openGL_기초(화면 터치이벤트에 따른 삼각형 회전)

--- app/src/main/java/com/example/studyanimation/image_graphic/opengl/MyGLRenderer.kt
+++ app/src/main/java/com/example/studyanimation/image_graphic/opengl/MyGLRenderer.kt
@@ -3,10 +3,14 @@ package com.example.studyanimation.image_graphic.opengl
 import android.opengl.GLES20
 import android.opengl.GLSurfaceView
 import android.opengl.Matrix
+import android.os.SystemClock
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
 
 class MyGLRenderer : GLSurfaceView.Renderer {
+
+    @Volatile
+    var angle: Float = 0f
 
     private lateinit var mTriangle: Triangle
     private lateinit var mSquare: Square
@@ -14,6 +18,8 @@ class MyGLRenderer : GLSurfaceView.Renderer {
     private val vPMatrix = FloatArray(16)
     private val projectionMatrix = FloatArray(16)
     private val viewMatrix = FloatArray(16)
+
+    private val rotationMatrix = FloatArray(16)
 
     //참고: OpenGL ES 2.0 API를 사용할 때 이러한 메서드에 GL10 매개변수가 있는 이유를 궁금해할 수 있습니다.
     //이러한 메서드 서명은 단순히 2.0 API에서 Android 프레임워크 코드를 더 간단하게 유지하기 위해 재사용합니다.
@@ -27,13 +33,23 @@ class MyGLRenderer : GLSurfaceView.Renderer {
 
     override fun onDrawFrame(unused: GL10) {
 
+        val scratch = FloatArray(16)
+
+        // Create a rotation transformation for the triangle
+//        val time = SystemClock.uptimeMillis() % 4000L
+//        val angle = 0.090f * time.toInt()
+
+        Matrix.setRotateM(rotationMatrix, 0, angle, 0f, 0f, -1.0f)
+
         Matrix.setLookAtM(viewMatrix, 0, 0f, 0f, -3f, 0f, 0f, 0f, 0f, 1.0f, 0.0f)
 
         // Calculate the projection and view transformation
         Matrix.multiplyMM(vPMatrix, 0, projectionMatrix, 0, viewMatrix, 0)
 
+        Matrix.multiplyMM(scratch, 0, vPMatrix, 0, rotationMatrix, 0)
 
-        mTriangle.draw(vPMatrix)
+
+        mTriangle.draw(scratch)
     }
 
     override fun onSurfaceChanged(unused: GL10, width: Int, height: Int) {
--- app/src/main/java/com/example/studyanimation/image_graphic/opengl/MyGLSurfaceView.kt
+++ app/src/main/java/com/example/studyanimation/image_graphic/opengl/MyGLSurfaceView.kt
@@ -2,10 +2,14 @@ package com.example.studyanimation.image_graphic.opengl
 
 import android.content.Context
 import android.opengl.GLSurfaceView
+import android.view.MotionEvent
 
 class MyGLSurfaceView(context: Context) : GLSurfaceView(context) {
 
     private val renderer: MyGLRenderer
+
+    private var previousX: Float = 0f
+    private var previousY: Float = 0f
 
     init {
 
@@ -23,5 +27,44 @@ class MyGLSurfaceView(context: Context) : GLSurfaceView(context) {
         renderMode = RENDERMODE_WHEN_DIRTY
         //이처럼 설정하면 requestRender()를 호출할 때까지 GLSurfaceView 프레임을 다시 그리지 않습니다. 그러면 이 샘플 앱에 더 효율적입니다.
     }
+
+    override fun onTouchEvent(e: MotionEvent): Boolean {
+        // MotionEvent reports input details from the touch screen
+        // and other input controls. In this case, you are only
+        // interested in events where the touch position changed.
+
+        val x: Float = e.x
+        val y: Float = e.y
+
+        when (e.action) {
+            MotionEvent.ACTION_MOVE -> {
+
+                var dx: Float = x - previousX
+                var dy: Float = y - previousY
+
+                // reverse direction of rotation above the mid-line
+                if (y > height / 2) {
+                    dx *= -1
+                }
+
+                // reverse direction of rotation to left of the mid-line
+                if (x < width / 2) {
+                    dy *= -1
+                }
+
+                renderer.angle += (dx + dy) * TOUCH_SCALE_FACTOR
+                requestRender()
+            }
+        }
+
+        previousX = x
+        previousY = y
+        return true
+    }
+
+    companion object {
+        private const val TOUCH_SCALE_FACTOR: Float = 180.0f / 320f
+    }
+
 
 }
