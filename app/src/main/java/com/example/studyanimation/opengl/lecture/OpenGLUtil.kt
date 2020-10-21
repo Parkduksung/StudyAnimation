3ce7fa79afbdbf52f70baa5dc78e9403a19562f4
Parkduksung <duksung1234@naver.com>
2020-10-21 23:16:48 +0900

    openGL 기능분리 - util 을 만들어 하나씩 분리 작업진행 (program 만들기, shader 만들기)

---
+++ app/src/main/java/com/example/studyanimation/opengl/lecture/OpenGLUtil.kt
@@ -0,0 +1,41 @@
+package com.example.studyanimation.opengl.lecture
+
+import android.opengl.GLES20
+
+object OpenGLUtil {
+
+
+    fun loadShader(type: Int, shaderCode: String?): Int {
+        val shader = GLES20.glCreateShader(type)
+        GLES20.glShaderSource(shader, shaderCode)
+        GLES20.glCompileShader(shader)
+        return shader
+    }
+
+
+    fun createProgram(vertexSource: String, fragmentSource: String): Int {
+        val vertexShader = loadShader(GLES20.GL_VERTEX_SHADER, vertexSource)
+        if (vertexShader == 0) {
+            return 0
+        }
+        val pixelShader = loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentSource)
+        if (pixelShader == 0) {
+            return 0
+        }
+        var program = GLES20.glCreateProgram()
+
+        GLES20.glAttachShader(program, vertexShader)
+        GLES20.glAttachShader(program, pixelShader)
+        GLES20.glLinkProgram(program)
+
+        val linkStatus = IntArray(1)
+
+        GLES20.glGetProgramiv(program, GLES20.GL_LINK_STATUS, linkStatus, 0)
+        if (linkStatus[0] != GLES20.GL_TRUE) {
+            GLES20.glDeleteProgram(program)
+            program = 0
+        }
+        return program
+    }
+
+}
