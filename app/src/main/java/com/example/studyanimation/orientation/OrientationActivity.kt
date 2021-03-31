bb41446f0d3de04646bc0f070645f9c72b4e1b89
Parkduksung <duksung1234@naver.com>
2021-03-31 10:26:40 +0900

    [rotation] 고정된 화면에서 버튼을 클릭시에만 회전이 돌아가게 구현.

--- app/src/main/java/com/example/studyanimation/orientation/OrientationActivity.kt
+++ app/src/main/java/com/example/studyanimation/orientation/OrientationActivity.kt
@@ -2,15 +2,31 @@ package com.example.studyanimation.orientation
 
 import android.content.pm.ActivityInfo
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import com.example.studyanimation.BaseActivity
 import com.example.studyanimation.R
 
 class OrientationActivity : BaseActivity(R.layout.activity_orientation) {
+
+    private var toggleRotation = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LOCKED
 
+
+        findViewById<Button>(R.id.rotation_button).setOnClickListener {
+
+            toggleRotation = !toggleRotation
+
+            Log.d("결과", toggleRotation.toString())
+            requestedOrientation = if(!toggleRotation){
+                ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+            }else{
+                ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+            }
+        }
     }
 }
