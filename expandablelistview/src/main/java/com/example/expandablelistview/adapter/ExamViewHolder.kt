079b598651aecdb95d22201b23903b676d24aa19
Parkduksung <duksung1234@naver.com>
2022-01-01 22:32:05 +0900

    [feature] binding 방식 변경.

--- expandablelistview/src/main/java/com/example/expandablelistview/adapter/ExamViewHolder.kt
+++ expandablelistview/src/main/java/com/example/expandablelistview/adapter/ExamViewHolder.kt
@@ -11,22 +11,19 @@ import com.example.expandablelistview.databinding.ItemExamBinding
 class ExamViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
     LayoutInflater.from(parent.context).inflate(R.layout.item_exam, parent, false)
 ) {
-    private val binding = DataBindingUtil.bind<ItemExamBinding>(itemView)
+    private val binding = ItemExamBinding.bind(itemView)
 
     fun bind(item: Exam, listener: (item: Exam) -> Unit) {
-
-        binding?.let {
-            with(it) {
-                no.text = item.no.toString()
-                name.text = item.name
-                memo.text = item.memo
-                expand.text = item.isExpand.toString()
-                binding.containerExpand.isVisible = item.isExpand
-                if (binding.containerExpand.isVisible) {
-                    binding.containerExpand.animate().setDuration(200L).rotation(360f)
-                } else {
-                    binding.containerExpand.animate().setDuration(200L).rotation(0f)
-                }
+        with(binding) {
+            no.text = item.no.toString()
+            name.text = item.name
+            memo.text = item.memo
+            expand.text = item.isExpand.toString()
+            binding.containerExpand.isVisible = item.isExpand
+            if (binding.containerExpand.isVisible) {
+                binding.containerExpand.animate().setDuration(200L).rotation(360f)
+            } else {
+                binding.containerExpand.animate().setDuration(200L).rotation(0f)
             }
         }
 
