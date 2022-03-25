ccb64a674c5e914ccd2715011355f21d0507c914
Parkduksung <duksung1234@naver.com>
2022-03-26 00:27:09 +0900

    [feature] 앱 사용 시간 알수 있는 방법.

--- usage/src/main/java/com/example/usage/MainActivity.kt
+++ usage/src/main/java/com/example/usage/MainActivity.kt
@@ -1,11 +1,58 @@
 package com.example.usage
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.AppOpsManager
+import android.app.AppOpsManager.MODE_ALLOWED
+import android.app.AppOpsManager.OPSTR_GET_USAGE_STATS
+import android.app.usage.UsageStats
+import android.app.usage.UsageStatsManager
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.os.Process.myUid
+import android.provider.Settings
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (!checkForPermission()) {
+            startActivity(Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS))
+        } else {
+            showAppUsageStats(getAppUsageStats())
+        }
+    }
+
+    private fun checkForPermission(): Boolean {
+        val appOps = getSystemService(Context.APP_OPS_SERVICE) as AppOpsManager
+        val mode = appOps.checkOpNoThrow(OPSTR_GET_USAGE_STATS, myUid(), packageName)
+        return mode == MODE_ALLOWED
+    }
+
+    private fun getAppUsageStats(): MutableList<UsageStats> {
+        val cal = Calendar.getInstance()
+        cal.add(Calendar.YEAR, -1)    // 1
+
+        val usageStatsManager =
+            getSystemService(Context.USAGE_STATS_SERVICE) as UsageStatsManager // 2
+        return usageStatsManager.queryUsageStats(
+            UsageStatsManager.INTERVAL_DAILY, cal.timeInMillis, System.currentTimeMillis() // 3
+        )
+    }
+
+    private fun showAppUsageStats(usageStats: MutableList<UsageStats>) {
+        usageStats.sortWith { right, left ->
+            compareValues(left.lastTimeUsed, right.lastTimeUsed)
+        }
+
+        usageStats.forEach {
+            Log.d(
+                "결과", "packageName: ${it.packageName}, lastTimeUsed: ${Date(it.lastTimeUsed)}, " +
+                        "totalTimeInForeground: ${it.totalTimeInForeground}"
+            )
+        }
     }
 }
